6aa746ccdc8ef6edd5f5c3c3b0603d0879899f93
gahyeon <1000ji1@naver.com>
2023-10-21 00:54:24 +0900

    CLN/Code Organization

--- CSDServer/CSDServer/settings/production.py
+++ CSDServer/CSDServer/settings/production.py
@@ -5,7 +5,7 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False  # 운영하는 서버
 
-ALLOWED_HOSTS = ['54.180.116.175', '*']
+ALLOWED_HOSTS = ['*']
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
